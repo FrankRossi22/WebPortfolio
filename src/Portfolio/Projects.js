ae6f2e7064e4584314f83abd836245f919557032
Frank Rossi <rossi.229@osu.edu>
2024-07-24 16:57:33 -0400

    a

--- src/Portfolio/Projects.js
+++ src/Portfolio/Projects.js
@@ -60,7 +60,7 @@ function Projects({elRef, screenSize}) {
         {link: 'https://github.com/FrankRossi22/LunchCount2024', icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg', name: 'GitHub'}
       ]}
       description={"With the large growth of LMS (Learning Management Systems) over the past decade, " 
-        + "Lunch Count forms as a building block to improving these platforms by providing a robust system for tracking student lunch habits and orders."
+        + "Lunch Count serves as a building block to improving these platforms by providing a robust system for tracking student lunch orders."
         + "Users (Students, Teachers, and Admin) each have their own dashboard with admin having sole access to lunch creation and updating. This was created using JS, Express, and NedB Databases."} 
       image={"./images/lunchCount.png"}/>
       <Project name='Baja Buckeyes' createdWith={bajaMadeWith} id={'BB'} links={[
