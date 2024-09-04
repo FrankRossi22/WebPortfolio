a553e899c2a78d4f3f61c156a7dc3f7926ab6fdc
Frank Rossi <rossi.229@osu.edu>
2024-09-03 20:03:48 -0400

    s

--- src/Portfolio/Experience.js
+++ src/Portfolio/Experience.js
@@ -10,7 +10,7 @@ function Experience({elRef}) {
         "Worked under the director of city services to create a cleaner and more efficient website to direct traffic to important pages and landmarks",
         "Assisted with tech and development issues for the office"
         ]}/>
-      <Job jobTitle='Social Media Manager' location={'Smokers Paradise'} id={1} year={"June 2023 - April 2024"}
+      <Job jobTitle='Social Media Manager' location={'Smokers Paradise'} id={1} year={"June 2022 - April 2024"}
       description={["Design and execute social media posts to inform and draw in new and returning customers",
         "Engage in regular coordination with the store owner, jointly devising sales strategies and creating sales content",
         "Use Adobe Photoshop and Adobe Premiere to create posts about store events, sales, and closures"
